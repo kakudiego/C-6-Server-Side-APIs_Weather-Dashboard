b6f8204219c13831aea853f23daecdb98aac050e
Diego Kaku <diegokaku@gmail.com>
2022-01-31 00:53:27 -0600

    change html, add delete history button, setItem and getItem working, need to fix search, need to fix 1 item only local storage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,50 +3,58 @@
 // display city name + date + weather icon + temp + humidity + wind speed + uv index
 // weather condition represented by color (favorable, moderate, or severe)
 
-var city = "cochabamba";
+var city = "cochabamba"; // change to data.name
+var lat = "17.3895"; // change to data.coord.lat
+var lon = "-66.1568"; // change to data.coord.lon
 
 var oneDay = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=61763921a1722d721341f9896cdced9f";
+var uvi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&appid=61763921a1722d721341f9896cdced9f";
 var fiveDays = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial" + "&appid=61763921a1722d721341f9896cdced9f";
 
-var uvi = "https://api.openweathermap.org/data/2.5/onecall?lat=-17.3895&lon=-66.1568&exclude=minutely,hourly&appid=61763921a1722d721341f9896cdced9f";
-// var uvIndex = (lat + lon)
-
 var getWeather = function (city) {
-  fetch(oneDay).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        console.log(data);
-        console.log("temp = " + data.main.temp);
-        console.log("humidity = " + data.main.humidity);
-        console.log("UV Index = " + data.main.humidity);
-        console.log("wind = " + data.wind.speed);
-        console.log("lat = " + data.coord.lat);
-        console.log("lon = " + data.coord.lon);
-      });
-    }
-  });
-};
-getWeather();
-
-var getWeather = function (city) {
-  fetch(fiveDays).then(function (response) {
-    response.json().then(function (data) {
-      console.log(data);
+  fetch(oneDay)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          console.log("today weather", data);
+          console.log("city = " + data.name);
+          console.log("temp = " + data.main.temp + " F");
+          console.log("humidity = " + data.main.humidity + " %");
+          console.log("wind = " + data.wind.speed + " MPH");
+          console.log("lat = " + data.coord.lat);
+          console.log("lon = " + data.coord.lon);
+          return;
+        });
+      }
+    })
+    .catch(function (err) {
+      console.log("Fetch Error :-S", err);
     });
-  });
 };
 getWeather();
 
 // UVI section
-var getUVI = function () {
+var getUVI = function (lat, lon) {
   fetch(uvi).then(function (response) {
     response.json().then(function (data) {
-      console.log(data);
+      console.log("UV Index = " + data.current.uvi);
+      console.log("UVI log", data);
     });
   });
 };
 getUVI();
 
+var nextFive = function (city) {
+  fetch(fiveDays).then(function (response) {
+    response.json().then(function (data) {
+      console.log("5 days", data);
+    });
+  });
+};
+nextFive();
+
+//
+//
 // display day and time
 // prototype - first time using
 var date = new Date();
@@ -65,17 +73,18 @@ $("#day4").text(date.addDays(3).toLocaleDateString("en-US"));
 $("#day5").text(date.addDays(4).toLocaleDateString("en-US"));
 $("#day6").text(date.addDays(5).toLocaleDateString("en-US"));
 
-// search section
-var search = $("#search");
+//
+//
+// Local Storage section
+// keep data after refresh page
+let saveBtn = document.querySelector("#searchbtn");
+let searchCity = document.querySelector("#search");
 
-var searchForm = function (event) {
+searchCity.value = JSON.parse(localStorage.getItem("city"));
+
+saveBtn.addEventListener("click", function (event) {
   event.preventDefault();
-  var cidade = search.value;
-  console.log(event);
-};
-
-$("#searchbtn").click(function (e) {
-  e.preventDefault();
+  localStorage.setItem("city", JSON.stringify(searchCity.value));
 });
 
 $("#search").keypress(function (event) {
@@ -84,3 +93,10 @@ $("#search").keypress(function (event) {
     $("#searchbtn").click();
   }
 });
+
+// delete task button
+// input empty
+function deleteCity() {
+  localStorage.removeItem("city");
+  searchCity.value = "";
+}
